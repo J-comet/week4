c656f685fd160744b2ab1c9c581281a509df5773
hyeseong <hyeseong7848@gmail.com>
2023-08-07 12:26:58 +0900

    git ignore 파일 추가

--- week4/ViewController.swift
+++ week4/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
+    let url = "http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230111"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        callRequest()
     }
 
 
+    private func callRequest() {
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
 
